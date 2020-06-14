a941fc82c01d34e00d7842b429295b63e14c6646
runner-lxj <15339384801@163.com>
2020-06-14 21:53:48 +0800

    第三次提交

--- HW2020/scripts.js
+++ HW2020/scripts.js
@@ -1,4 +1,10 @@
-function renderEditor{
+
+let tasks = []; //{title:"dddddd",done:flase}
+
+
+
+
+function renderEditor() {
     let inputEl = document.querySelector("#default-todo-panel .todo-editor > input");
     //inputEl.onchange = (e) => {
     //    console.log("text, ", e.target.value);
@@ -7,10 +13,42 @@ function renderEditor{
     let addEl = document.querySelector("#default-todo-panel .todo-editor > button");
     addEl.onclick = (e) => {
         console.log("add click");
+        let newTask = {
+            title: inputEl.value,
+            done: false,
+        };
+        tasks.push(newTask);
+
+        console.log("tasks: ",tasks);
+
+        renderTaskItems();
     };
     
     }
     
+    function renderTaskItems(){
+        let itemsEl = document.querySelector("#default-todo-panel .todo-items");
+
+        for (let i = 0; i < tasks.length; i++ ){
+            let task = tasks[i];
+            let itemEl = document.createElement("div");
+            
+
+
+            let doneEl = document.createElement("input");
+            doneEl.type = "checkbox";
+            itemEl.append(doneEl);
+
+            let titleEl = document.createElement("label");
+            titleEl.innerText = task.title;
+            itemEl.append(titleEl);
+            let cancelEl = document.createElement("button");
+            cancelEl.innerText = "X";
+            itemEl.append(cancelEl);
+
+            itemsEl.append(itemEl);
+        }
+    }
     
     
-    renderEdito();
+    renderEditor();
