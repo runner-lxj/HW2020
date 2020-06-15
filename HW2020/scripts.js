716435332c59d4d22e9e3b373457351ec5478f48
runner-lxj <15339384801@163.com>
2020-06-15 19:17:14 +0800

    第五次提交

--- HW2020/scripts.js
+++ HW2020/scripts.js
@@ -11,68 +11,118 @@ function renderEditor() {
     //    //console.log("input change: ", e);
     // };
     inputEl.onkeypress = (e) => {
-        
-        if (e.key==="Enter"){
-           addTask(); 
+
+        if (e.key === "Enter") {
+            addTask();
         };
     };
 
-    let addTask = () =>{
+    let addTask = () => {
+        if (inputEl.value.length === 0) {
+            return;
+        }
         let newTask = {
             title: inputEl.value,
             done: false,
         };
         inputEl.value = "";
         tasks.push(newTask);
-        
 
-        console.log("tasks: ",tasks);
+
+        console.log("tasks: ", tasks);
 
         renderTaskItems();
- 
+
     };
-    
+
 
 
     let addEl = document.querySelector("#default-todo-panel .todo-editor > button");
     addEl.onclick = (e) => {
         addTask();
-    
+
     };
 }
-    
-    function renderTaskItems(){
-        console.log("render items");
-        let itemsEl = document.querySelector("#default-todo-panel .todo-items");
-        itemsEl.querySelectorAll("div").forEach((node)=>node.remove());
+
+function renderTaskItems() {
+    console.log("render items");
+    let itemsEl = document.querySelector("#default-todo-panel .todo-items");
+    itemsEl.querySelectorAll("div").forEach((node) => node.remove());
 
 
-        for (let i = 0; i < tasks.length; i++ ){
-            let task = tasks[i];
-            let itemEl = document.createElement("div");
-            
+    for (let i = 0; i < tasks.length; i++) {
+        let task = tasks[i];
+        let itemEl = document.createElement("div");
+        itemEl.className = "task";
 
 
-            let doneEl = document.createElement("input");
-            doneEl.type = "checkbox";
-            itemEl.append(doneEl);
 
-            let titleEl = document.createElement("label");
-            titleEl.innerText = task.title;
-            itemEl.append(titleEl);
-
-            let cancelEl = document.createElement("button");
-            cancelEl.innerText = "X";
-            cancelEl.onclick = () =>{
-                tasks.splice(i, 1);
-                renderTaskItems();
-            }
-            itemEl.append(cancelEl);
-
-            itemsEl.append(itemEl);
+        let doneEl = document.createElement("input");
+        doneEl.type = "checkbox";
+        doneEl.checked = task.done;
+        if (task.done) {
+            itemEl.classList.add("done");
+        } else {
+            itemEl.classList.remove("done");
         }
+        doneEl.onchange = (e) => {
+            task.done = e.target.checked;
+            //console.log("checkbox: ", e);
+            if (task.done) {
+                itemEl.classList.add("done");
+            } else {
+                itemEl.classList.remove("done");
+            }
+        }
+        itemEl.append(doneEl);
+
+        let titleEl = document.createElement("label");
+        titleEl.innerText = task.title;
+        itemEl.append(titleEl);
+
+
+        let ctrlbarEl = renderTaskCtrlBar(tasks, i);
+
+        itemEl.append(ctrlbarEl);
+
+        itemsEl.append(itemEl);
     }
-    
-    
-    renderEditor();
-    renderTaskItems();
+}
+
+function renderTaskCtrlBar(tasks, taskIdx) {
+    let ctrlbarEl = document.createElement("div");
+    ctrlbarEl.className = "ctrlbar";
+
+
+
+    let upEl = document.createElement("button");
+    if (taskIdx === 0) {
+        upEl.disabled = true;
+    }
+    upEl.innerText = "🠑";
+    upEl.onclick = () => {
+        //
+    };
+    ctrlbarEl.append(upEl);
+
+    let downEl = document.createElement("button");
+    downEl.innerText = "🠓";
+    downEl.onclick = () => {
+        //
+    };
+    ctrlbarEl.append(downEl);
+
+
+    let cancelEl = document.createElement("button");
+    cancelEl.innerText = "X";
+    cancelEl.onclick = () => {
+        tasks.splice(taskIdx, 1);
+        renderTaskItems();
+    }
+
+    ctrlbarEl.append(cancelEl);
+
+    return ctrlbarEl;
+}
+renderEditor();
+renderTaskItems();
